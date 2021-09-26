8de0db3e289e31cccd072b66946c9ffe184861b5
akourk <akourk@icloud.com>
2021-09-26 13:53:33 -0700

    reddit clone backend client- bootspring

---
+++ demo/src/main/java/com/example/redditclone/exceptions/SpringRedditException.java
@@ -0,0 +1,12 @@
+package com.example.redditclone.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+}
